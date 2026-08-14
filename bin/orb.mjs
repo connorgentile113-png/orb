c4a127e71f2e545bba4876f137a510694b3706c2
Connor Gentile <connorgentile113@gmail.com>
2026-08-14 13:09:15 -0400

    Release Orb 0.13.0

--- bin/orb.mjs
+++ bin/orb.mjs
@@ -315,7 +315,7 @@ async function main() {
   const args = process.argv.slice(2);
   const command = args.shift() || '';
   if (['help', '--help', '-h'].includes(command)) return stdout.write(usage());
-  if (command === '--version' || command === '-v') return stdout.write('orb 0.12.0\n');
+  if (command === '--version' || command === '-v') return stdout.write('orb 0.13.0\n');
   const config = loadConfig();
   if (!command) {
     if (!stdin.isTTY) return stdout.write(usage());
